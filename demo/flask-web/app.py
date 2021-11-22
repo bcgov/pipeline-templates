662af1397d7c86c5df16abb4be47e4a9491c2a97
Greg Robinson <gregnrobinson@gmail.com>
2021-11-21 22:25:33 -0500

    cleanup

--- demo/flask-web/app.py
+++ demo/flask-web/app.py
@@ -1,14 +1,3 @@
-
-import time
-import redis
-import platform
-import socket
-import re
-import uuid
-import json
-import psutil
-import logging
-
 from flask import Flask, json, render_template
 
 app = Flask(__name__)
