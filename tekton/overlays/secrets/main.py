f48a45f9837684b2795c61c28b60e86daa80afc6
Greg Robinson <gregnrobinson@gmail.com>
2021-12-12 00:29:41 -0500

    release 1.0.0

---
+++ tekton/overlays/secrets/main.py
@@ -0,0 +1,92 @@
+import pystache
+import sys
+import os
+import shutil
+import configparser
+from collections import OrderedDict
+
+os.chdir("overlays/secrets")
+
+Config = configparser.ConfigParser()
+try:
+    Config.read("secrets.ini")
+except Exception:
+    pass
+  
+# Order the content of DEFAULT section alphabetically
+Config._defaults = OrderedDict(
+    sorted(Config._defaults.items(), key=lambda t: t[0]))
+
+# Order the content of each section alphabetically
+for section in Config._sections:
+    Config._sections[section] = OrderedDict(
+        sorted(Config._sections[section].items(), key=lambda t: t[0]))
+
+# Order all sections alphabetically
+Config._sections = OrderedDict(
+    sorted(Config._sections.items(), key=lambda t: t[0]))
+
+ssh      = Config._sections['ssh']
+literals = Config._sections['literals']
+docker   = Config._sections['docker']
+
+try:
+    os.makedirs(".secrets")
+except FileExistsError:
+    pass
+for key, value in ssh.items():
+    new_path = shutil.copy(value, '.secrets')
+    ssh[key] = new_path
+for key, value in docker.items():
+    new_path = shutil.copy(value, '.secrets/.dockerconfigjson')
+    docker[key] = new_path
+
+input = """apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+generatorOptions:
+  disableNameSuffixHash: true
+secretGenerator:
+{{#literals}}
+- name: {{k}}
+  type: Opaque
+  literals:
+    - {{k}}={{v}}
+{{/literals}}
+{{#ssh}}
+- name: {{k}}
+  files:
+    - {{v}}
+{{/ssh}}
+{{#docker}}
+- name: {{k}}
+  files:
+    - {{v}}
+  type: kubernetes.io/dockerconfigjson
+{{/docker}}
+"""
+
+literals = [{"k": k, "v": v} for k, v in literals.items()]
+ssh      = [{"k": k, "v": v} for k, v in ssh.items()]
+docker   = [{"k": k, "v": v} for k, v in docker.items()]
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(bcolors.OKCYAN + "Writing secrets to Kustomize..." + bcolors.ENDC)
+
+original_stdout = sys.stdout
+
+with open('kustomization.yaml', 'w') as f:
+    sys.stdout = f
+    print(pystache.render(input, {"literals": literals, "ssh": ssh, "docker": docker}))
+    sys.stdout = original_stdout
+    
+print(bcolors.OKCYAN + "Completed..." + bcolors.ENDC)
---
+++ tekton/tekton.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+export PATH=$PATH:${PWD}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export cyan=$(tput setaf 6)
+export green=$(tput setaf 2)
+export warn=$(tput setaf 3)
+export bold=$(tput bold)
+export normal=$(tput sgr0)
+
+function apply(){
+    secrets
+    if [ -z "${CONTEXT}" ]; then
+        kubectl apply -k overlays/apply || prune
+    else
+        kubectl config use-context ${CONTEXT}
+        kubectl apply -k overlays/apply || prune
+    fi
+    echo "${green}Apply completed successfully...${normal}"
+}
+
+function secrets(){
+    pip install -r ${DIR}/overlays/secrets/requirements.txt --exists-action i --quiet
+    python ${DIR}/overlays/secrets/main.py
+
+    if [ -z "${CONTEXT}" ]; then
+        kubectl apply -k overlays/secrets
+    else
+        kubectl config use-context ${CONTEXT}
+        kubectl apply -k overlays/secrets
+    fi
+
+    rm -rf overlays/secrets/kustomization.yaml
+    rm -rf overlays/secrets/.secrets
+    echo "${green}Secrets configured successfully...${normal}"
+}
+
+function prune(){
+    setup
+    kubectl delete pipelinerun $(kubectl get pipelinerun | awk '{print $1}') 2> /dev/null || echo "${green}Pruning completed successfully...${normal}"
+}
+
+function display_help() {
+    SHORT_SHA="$(git rev-parse --short HEAD)"
+    BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    echo ""
+    echo "${cyan}Tekton Kustomize CLI${normal}"
+    echo "${cyan}Version: ${BRANCH}-${SHORT_SHA}${normal}"
+    echo ""
+    echo "Usage: tekton.sh [option...]" >&2
+    echo
+    echo "   ${bold}-a, --apply${normal}         Apply secrets, pipelines, tasks, and triggers. "
+    echo "   ${bold}-p, --prune${normal}         Delete all ${bold}Completed${normal}, $(tput bold)Errored${normal} or $(tput bold)DeadLineExceeded${normal} pod runs. "
+    echo "   ${bold}-h, --help${normal}          Display argument options. "
+    echo
+    exit 1
+}
+
+while :
+do
+    case "$1" in
+      -h | --help)
+          display_help
+          exit 0
+          ;;
+      -a | --apply)
+          apply
+          shift 2
+          ;;
+      -p | --prune)
+          prune
+          shift 2
+          ;;
+
+      --) # End of all options
+          shift
+          break
+          ;;
+      -*)
+          echo "Error: Unknown option: $1" >&2
+          ## or call function display_help
+          exit 1
+          ;;
+      *)  # No more options
+          break
+          ;;
+    esac
+done
+
+case "$1" in
+  *)
+     display_help
+
+     exit 1
+     ;;
+esac
