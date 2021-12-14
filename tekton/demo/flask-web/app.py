5dc32b7d330fa7ee2b6249cbbf6d9d189eec1ddf
Greg Robinson <gregnrobinson@gmail.com>
2021-12-13 21:02:46 -0500

    Added quote generator

--- tekton/demo/flask-web/app.py
+++ tekton/demo/flask-web/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, json, render_template
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    url = "https://api.kanye.rest"
+    url = "https://api.quotable.io/random"
     data = requests.get(url)
     response = data.json()
-    quote = response["quote"]
-    return render_template("index.html", quote=quote)
+    quote = response["content"]
+    author = response["author"]
+    return render_template("index.html", quote=quote, author=author)
+
+if __name__ == "__main__":
+    app.run()
