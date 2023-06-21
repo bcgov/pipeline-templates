d07605803ecde918ff933404a17ffc10fd7aaa58
BillyLi <billy.li@gov.bc.ca>
2023-06-21 14:37:59 -0700

    update git-clone task to use PAT

--- tekton/overlays/secrets/main.py
+++ tekton/overlays/secrets/main.py
@@ -14,25 +14,29 @@ except Exception:
     pass
 
 # Order the content of DEFAULT section alphabetically
-Config._defaults = OrderedDict(sorted(Config._defaults.items(), key=lambda t: t[0]))
+Config._defaults = OrderedDict(
+    sorted(Config._defaults.items(), key=lambda t: t[0]))
 
 # Order the content of each section alphabetically
 for section in Config._sections:
-    Config._sections[section] = OrderedDict(sorted(Config._sections[section].items(), key=lambda t: t[0]))
+    Config._sections[section] = OrderedDict(
+        sorted(Config._sections[section].items(), key=lambda t: t[0]))
 
 # Order all sections alphabetically
-Config._sections = OrderedDict(sorted(Config._sections.items(), key=lambda t: t[0]))
+Config._sections = OrderedDict(
+    sorted(Config._sections.items(), key=lambda t: t[0]))
 
-ssh      = Config._sections['ssh']
+# ssh = Config._sections['ssh']
 literals = Config._sections['literals']
 
-try:
-    os.makedirs(".secrets")
-except FileExistsError:
-    pass
-for key, value in ssh.items():
-    new_path = shutil.copy(value, '.secrets')
-    ssh[key] = new_path
+# try:
+#     os.makedirs(".secrets")
+# except FileExistsError:
+#     pass
+# for key, value in ssh.items():
+#     if value:
+#         new_path = shutil.copy(value, '.secrets')
+#         ssh[key] = new_path
 
 input = """apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
@@ -45,15 +49,16 @@ secretGenerator:
   literals:
     - {{k}}={{v}}
 {{/literals}}
-{{#ssh}}
-- name: {{k}}
-  files:
-    - {{v}}
-{{/ssh}}
+# {{#ssh}}
+# - name: {{k}}
+#   files:
+#     - {{v}}
+# {{/ssh}}
 """
 
 literals = [{"k": k, "v": v} for k, v in literals.items()]
-ssh      = [{"k": k, "v": v} for k, v in ssh.items()]
+# ssh = [{"k": k, "v": v} for k, v in ssh.items()]
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -66,13 +71,14 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 print(bcolors.OKCYAN + "Writing secrets to Kustomize..." + bcolors.ENDC)
 
 original_stdout = sys.stdout
 
 with open('kustomization.yaml', 'w') as f:
     sys.stdout = f
-    print(pystache.render(input, {"literals": literals, "ssh": ssh}))
+    print(pystache.render(input, {"literals": literals}))
     sys.stdout = original_stdout
 
 print(bcolors.OKGREEN + "Completed..." + bcolors.ENDC)
